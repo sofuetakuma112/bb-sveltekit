bf374f0be54dddddda4867285b20189174898371
sofuetakuma <kawahagi0620@gmail.com>
2024-05-21 21:37:05 +0900

    より高速な@cf-wasm/photonに置き換え

--- src/routes/images/[type]/[name]/+server.ts
+++ src/routes/images/[type]/[name]/+server.ts
@@ -7,22 +7,18 @@ export const GET: RequestHandler = async (event) => {
   const name = event.params.name;
 
   // FIXME: ローカル開発でもwasm-image-optimizationからimportできるようにする
-  let optimizeImage
+  let photon;
   if (dev) {
-    optimizeImage = await import('wasm-image-optimization/node').then(
-      (module) => module.optimizeImage
-    );
+    photon = await import('@cf-wasm/photon/node');
   } else {
-    optimizeImage = await import('wasm-image-optimization').then(
-      (module) => module.optimizeImage
-    );
+    photon = await import('@cf-wasm/photon');
   }
 
   const queryParams = event.url.searchParams;
   const w = queryParams.get('w');
-  const q = queryParams.get('q');
+  // const q = queryParams.get('q');
   const width = w ? parseInt(w) : undefined;
-  const quality = q ? parseInt(q) : undefined;
+  // const quality = q ? parseInt(q) : undefined;
 
   const r2 = event.platform?.env.R2;
   if (r2 == null) {
@@ -37,15 +33,35 @@ export const GET: RequestHandler = async (event) => {
     });
   }
 
-  const buffer = await object.arrayBuffer();
-  const image = await optimizeImage({
-    image: buffer,
-    width: width,
-    quality: quality,
-    format: 'webp'
-  });
+  const imageBufer = await object.arrayBuffer()
+  const inputBytes = new Uint8Array(imageBufer)
 
-  return new Response(image, {
+  if (width == null) {
+    return new Response(inputBytes, {
+      headers: {
+        'Content-Type': 'image/webp'
+      }
+    });
+  }
+
+  // create a photon instance
+  const inputImage = photon.PhotonImage.new_from_byteslice(inputBytes);
+
+  // const aspectRatio = inputImage.get_width() / inputImage.get_height();
+  const ratio = width / inputImage.get_width();
+
+  // resize image using photon
+  const outputImage = photon.resize(inputImage, width, inputImage.get_height() * ratio, 1);
+
+  // get webp bytes
+  const outputBytes = outputImage.get_bytes_webp();
+
+  // for other formats
+  // png  : outputImage.get_bytes();
+  // jpeg : outputImage.get_bytes_jpeg(quality);
+
+  // return the Response instance
+  return new Response(outputBytes, {
     headers: {
       'Content-Type': 'image/webp',
       'Cache-Control': 'public, max-age=31536000, immutable'
